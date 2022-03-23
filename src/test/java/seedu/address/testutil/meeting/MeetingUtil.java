76b008b7102931090f9eb810b6e0c3278c371c4b
Chua Bing Quan <chipneverdies@gmail.com>
2022-03-23 21:10:49 +0800

    Revert "H2-1"
    This reverts commit 98f8ce5ffe37413abac2cf1f5d627ab09cab4af7.

--- src/test/java/seedu/address/testutil/meeting/MeetingUtil.java
+++ src/test/java/seedu/address/testutil/meeting/MeetingUtil.java
@@ -20,7 +20,7 @@ import seedu.address.model.meeting.Meeting;
  * This class is a placeholder for MeetingUtil. For reference please refer to the obsolete package.
  */
 public class MeetingUtil {
-    public static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("d-M-uuuu h:mma");
+    public final static DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("d-M-uuuu h:mma");
 
     public static String getAddMeetingCommand(Meeting meeting) {
         return AddMeetingCommand.COMMAND_WORD + " " + getMeetingDetails(meeting);
@@ -30,7 +30,7 @@ public class MeetingUtil {
         StringBuilder sb = new StringBuilder();
         sb.append(PREFIX_NAME + meeting.getName().name + " ");
         sb.append(PREFIX_URL + meeting.getUrl().toString() + " ");
-        sb.append(PREFIX_DATETIME + meeting.getDateTime().datetime.format(INPUT_FORMAT) + " ");
+        sb.append(PREFIX_DATETIME + meeting.getDateTime().datetime.format(inputFormat) + " ");
         sb.append(PREFIX_DURATION + String.valueOf(meeting.getDuration().duration) + " ");
         sb.append(PREFIX_MODULE + meeting.getModule().code + " ");
         sb.append(PREFIX_RECURRING + meeting.getIsRecurring().toString() + " ");
