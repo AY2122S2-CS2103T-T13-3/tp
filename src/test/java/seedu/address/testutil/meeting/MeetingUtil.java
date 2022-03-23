fae6fc18b12daf1a8182a313fb6823af530c2545
Chua Bing Quan <chipneverdies@gmail.com>
2022-03-23 21:10:48 +0800

    Revert "H3"
    This reverts commit c39b23dfd7e6e9b1b0df8600d4a7cfc7b4f8aa26.

--- src/test/java/seedu/address/testutil/meeting/MeetingUtil.java
+++ src/test/java/seedu/address/testutil/meeting/MeetingUtil.java
@@ -9,7 +9,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_URL;
 //import static seedu.address.model.meeting.MeetingDateTime.INPUT_FORMAT;
 
-//import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatter;
 
 import seedu.address.logic.commands.meeting.AddMeetingCommand;
 import seedu.address.model.meeting.Meeting;
@@ -20,7 +20,7 @@ import seedu.address.model.meeting.Meeting;
  * This class is a placeholder for MeetingUtil. For reference please refer to the obsolete package.
  */
 public class MeetingUtil {
-    //    public static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("d-M-uuuu h:mma");
+    public static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("d-M-uuuu h:mma");
 
     public static String getAddMeetingCommand(Meeting meeting) {
         return AddMeetingCommand.COMMAND_WORD + " " + getMeetingDetails(meeting);
@@ -30,7 +30,7 @@ public class MeetingUtil {
         StringBuilder sb = new StringBuilder();
         sb.append(PREFIX_NAME + meeting.getName().name + " ");
         sb.append(PREFIX_URL + meeting.getUrl().toString() + " ");
-        sb.append(PREFIX_DATETIME + meeting.getDateTime().datetime.toString() + " ");
+        sb.append(PREFIX_DATETIME + meeting.getDateTime().datetime.format(INPUT_FORMAT) + " ");
         sb.append(PREFIX_DURATION + String.valueOf(meeting.getDuration().duration) + " ");
         sb.append(PREFIX_MODULE + meeting.getModule().code + " ");
         sb.append(PREFIX_RECURRING + meeting.getIsRecurring().toString() + " ");
